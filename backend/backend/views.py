6c9309441c3700df7ec09e36672320f2a3579731
Sean Li <cli049@uottawa.ca>
2024-10-11 19:24:34 -0400

    Updated

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,8 @@
+import os
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    template_path = os.path.join("..", "frontend", "build", "index.html")
+    return render(request, template_path)
