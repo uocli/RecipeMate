a4e83bfa02f31ca86a05161955e926e812b84caa
Sean Li <cli049@uottawa.ca>
2024-11-07 14:07:04 -0500

    Added Chat History

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
 import Home from "./Home";
@@ -8,6 +8,7 @@ import NotFound from "./NotFound";
 import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import ProtectedRoute from "../utils/ProtectedRoute";
+import Chat from "./Chat";
 import UserProfile from "./UserProfile";
 import LogoutPage from "./Logout";
 import PasswordRecoveryForm from "./PasswordRecoveryForm";
@@ -36,6 +37,7 @@ const App = () => {
                 />
                 <Route path="/signup" element={<RegisterForm />} />
                 <Route path="/login" element={<LoginForm />} />
+                <Route path="/chat" element={<Chat history={chatHistory} />} />
                 <Route path="/logout" element={<LogoutPage />} />
                 <Route
                     path="/password-recovery"
