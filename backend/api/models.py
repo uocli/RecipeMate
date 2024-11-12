c911c98aadfe432da47f4c170f7863dd2a172809
Xinye130 <xzhu019@uottawa.ca>
2024-11-12 16:34:16 -0500

    Updated ShoppingListItem quantity to be int

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class ShoppingListItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient = models.CharField(max_length=255)
-    quantity = models.CharField(max_length=255)
+    quantity = models.IntegerField()
 
     def __str__(self):
         return f"{self.ingredient} - {self.quantity}"
