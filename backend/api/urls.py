6dbd340936ccfd98b92dcdc4bf7ea7095473332d
Xinye130 <xzhu019@uottawa.ca>
2024-11-12 15:47:39 -0500

    Added get_shopping_list view and shopping_list url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import get_csrf_token
+from .views import get_shopping_list
 
 urlpatterns = [
     path("csrf-token/", get_csrf_token, name="csrf-token"),
+    path('shopping-list/', get_shopping_list, name='shopping-list'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib.auth.decorators import login_required
+from .models import ShoppingListItem
 
 @csrf_exempt
 def get_csrf_token(request):
@@ -9,3 +10,12 @@ def get_csrf_token(request):
     response = JsonResponse({"csrfToken": token})
     response.set_cookie("csrftoken", token)
     return response
+
+@login_required
+def get_shopping_list(request):
+    user = request.user
+    shopping_list = ShoppingListItem.objects.filter(user=user)
+    data = [{"ingredient": item.ingredient, "quantity": item.quantity} for item in shopping_list]
+    print(f"User {user.username} retrieved their shopping list.")
+    print(data)
+    return JsonResponse(data, safe=False)
