d405f515abcaf67ac2b770773af97c245c8d0ce5
Xinye130 <xzhu019@uottawa.ca>
2024-11-12 15:44:05 -0500

    Added ShoppingListItem model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ShoppingListItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ingredient} - {self.quantity}"
