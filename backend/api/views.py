87a6530d313e0111ab82758db0620f91fdf38a0a
Xinye130 <xzhu019@uottawa.ca>
2024-11-13 00:27:14 -0500

    Added handle deletion for update_shopping_list

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,7 +17,7 @@ def get_csrf_token(request):
 def get_shopping_list(request):
     user = request.user
     shopping_list = ShoppingListItem.objects.filter(user=user).order_by('is_owned')
-    data = [{"ingredient": item.ingredient, "quantity": item.quantity, "is_owned": item.is_owned} for item in shopping_list]
+    data = [{"id": item.id, "ingredient": item.ingredient, "quantity": item.quantity, "unit": item.unit, "is_owned": item.is_owned} for item in shopping_list]
     print(f"User {user.username} retrieved their shopping list.")
     print(data)
     return JsonResponse(data, safe=False)
@@ -32,16 +32,19 @@ def update_shopping_list(request):
 
     if items:
         for item_data in items:
-            item_name = item_data.get('ingredient')
+            item_id = item_data.get('id')
             new_quantity = item_data.get('quantity')
             is_owned = item_data.get('is_owned')
-            if item_name and new_quantity and is_owned is not None:
+            if item_id and new_quantity is not None and is_owned is not None:
                 try:
-                    item = ShoppingListItem.objects.get(ingredient=item_name, user=user)
-                    item.quantity = new_quantity
-                    item.is_owned = is_owned
-                    item.save()
+                    item = ShoppingListItem.objects.get(id=item_id, user=user)
+                    if new_quantity == 0:
+                        item.delete()
+                    else:
+                        item.quantity = new_quantity
+                        item.is_owned = is_owned
+                        item.save()
                 except ShoppingListItem.DoesNotExist:
-                    return JsonResponse({"error": f"Item with id {item_name} not found"}, status=404)
+                    return JsonResponse({"error": f"Item with id {item_id} not found"}, status=404)
         return JsonResponse({"success": True})
     return JsonResponse({"error": "Invalid data"}, status=400)
