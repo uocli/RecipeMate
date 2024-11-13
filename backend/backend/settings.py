ed7876082dd93a9a168ce84fb6a2f1e8393719a5
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-11-13 08:23:38 -0500

    Update settings.py
    Fixed the issue where makemigrations command was not working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,16 +107,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.urls.py.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.urls.py.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.urls.py.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.urls.py.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
