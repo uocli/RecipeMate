05ace5aae198c0584feef49f712b77675fb9f230
Xinye130 <xzhu019@uottawa.ca>
2024-11-13 00:26:31 -0500

    Added unit attribute to ShoppingListItem

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,8 @@ class ShoppingListItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient = models.CharField(max_length=255)
     quantity = models.IntegerField()
+    unit = models.CharField(max_length=255, null=True, blank=True)
     is_owned = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.ingredient} - {self.quantity}"
+        return f"{self.ingredient} - {self.quantity} {self.unit} - {self.is_owned}"
