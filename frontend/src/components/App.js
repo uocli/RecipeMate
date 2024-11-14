0e66e3d70847b431c49da36b0ff0bf0e8324d157
Sean Li <cli049@uottawa.ca>
2024-11-14 08:32:08 -0500

    Added UserProfile.js to connect with the backend logic
    # Conflicts:
    #	frontend/src/components/App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,6 +8,7 @@ import NotFound from "./NotFound";
 import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import ProtectedRoute from "../utils/ProtectedRoute";
+import UserProfile from "./UserProfile";
 import LogoutPage from "./Logout";
 import PasswordRecoveryForm from "./PasswordRecoveryForm";
 
@@ -22,6 +23,14 @@ const App = () => {
                     element={
                         <ProtectedRoute>
                             <About />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <UserProfile />
                         </ProtectedRoute>
                     }
                 />
