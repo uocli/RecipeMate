1680c1355d46d9d9ee97bec163d8d381d9b2408c
Sean Li <cli049@uottawa.ca>
2024-11-14 08:13:23 -0500

    Added UserProfile model

---
+++ backend/api/custom_models/user_profile_models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    dietary_preference = models.CharField(
+        max_length=20,
+        choices=(
+            ("Vegetarian", "Vegetarian"),
+            ("Vegan", "Vegan"),
+            ("Gluten Free", "Gluten Free"),
+        ),
+        blank=True,
+        null=True,
+    )
+
+    # Add more fields as needed
+
+    def __str__(self):
+        return self.user.username
