2c05ee6f02a95f774a151ec75ed46c3ed2d7ce72
Sean Li <cli049@uottawa.ca>
2024-11-14 15:00:04 -0500

    Added User Acronym for frontend to show the avatar

--- backend/auth/serializers/user_serializer.py
+++ backend/auth/serializers/user_serializer.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    acronym = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["email", "password", "username"]
+        fields = ["email", "first_name", "last_name", "acronym"]
+
+    def get_acronym(self, obj):
+        first_initial = obj.first_name[0].upper() if obj.first_name else ""
+        last_initial = obj.last_name[0].upper() if obj.last_name else ""
+
+        return f"{first_initial}{last_initial}"
