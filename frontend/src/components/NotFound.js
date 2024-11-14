e97ba913861d3dd51222d672acfd62c41dbe4933
Sean Li <cli049@uottawa.ca>
2024-11-13 20:21:51 -0500

    Beautified 404 Page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,9 +1,33 @@
+import { Button, Container, Grid2 as Grid, Typography } from "@mui/material";
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-        </div>
-    );
+        <Container component="main" maxWidth="xs" sx={{ textAlign: 'center', marginTop: 8 }}>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12}>
+                    <Typography variant="h1" color="primary" sx={{ fontSize: '6rem', fontWeight: 'bold' }}>
+                        404
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h6" color="textSecondary" sx={{ marginBottom: 2 }}>
+                        Oops! The page doesn’t exist.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                        component={Link}
+                        to="/"
+                        variant="contained"
+                        color="primary"
+                        sx={{ padding: '10px 20px', fontSize: '1rem' }}
+                    >
+                        Go to Home
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>)
 };
 
 export default NotFound;
