9615937260305efe2237474ee8e5c416da1d62c9
Sean Li <cli049@uottawa.ca>
2024-11-14 07:56:31 -0500

    Injected the "from" location into the state of the ProtectedRoute.js, in which way, the user will be directed to their original requested url

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,12 +1,14 @@
-import React, { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
     const { isAuthenticated } = useContext(AuthContext);
+    const location = useLocation();
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace />;
+        // Redirect to login page with the original location saved in state
+        return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
     return children;
