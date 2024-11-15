522d0117d484ab53d867f48c48d55f129d63106f
Sean Li <cli049@uottawa.ca>
2024-11-15 11:24:41 -0500

    Added put for dietary preference and cocking time for User Profile

--- backend/api/custom_views/user_profile_views.py
+++ backend/api/custom_views/user_profile_views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from ..custom_models.user_profile_models import UserProfile
 from ..serializers.user_serializers import UserSerializer
 
 
@@ -23,6 +24,29 @@ class UserProfileView(APIView):
         user.first_name = request.data.get("first_name", user.first_name)
         user.last_name = request.data.get("last_name", user.last_name)
         user.save()
+        return Response(
+            {"user": self.user_serializer_class(user).data}, status.HTTP_200_OK
+        )
+
+    def put(self, request):
+        user = request.user
+        profile = UserProfile.objects.filter(user=user).first()
+        if profile is None:
+            profile = UserProfile.objects.create(
+                user=user,
+                dietary_preference=request.data.get("dietary_preference", ""),
+                cooking_time=request.data.get("cooking_time", ""),
+            )
+            profile.save()
+        else:
+            profile.dietary_preference = request.data.get(
+                "dietary_preference", profile.dietary_preference
+            )
+            profile.cooking_time = request.data.get(
+                "cooking_time", profile.cooking_time
+            )
+            profile.save(update_fields=["dietary_preference", "cooking_time"])
+
         return Response(
             {"user": self.user_serializer_class(user).data}, status.HTTP_200_OK
         )
