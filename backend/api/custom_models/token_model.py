96abbb3098a1f0f45d5e54e1edf0b4243580fa66
Sean Li <cli049@uottawa.ca>
2024-11-16 16:16:38 -0500

    Updated model Token

--- backend/api/custom_models/token_model.py
+++ backend/api/custom_models/token_model.py
@@ -10,5 +10,5 @@ class Token(models.Model):
     token = models.CharField(max_length=255)
     created_at = models.DateTimeField()
     expires_at = models.DateTimeField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tokens")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tokens")
     is_used = models.BooleanField(default=False)
