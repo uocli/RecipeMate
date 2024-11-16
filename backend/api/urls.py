e48c742c8475f0682c07f8a00088ded217b26f07
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-11-16 17:27:08 -0500

    Dev/21 fix auth issue (#45)
    * Added useAxios hook to help manage the tokens
    
    * Updated ProtectedRoute.js to verify token when a protected view is accessed
    
    * Updated AuthContext.js to handle tokens
    
    * Updated LoginForm.js to have the token passed into login method
    
    * Downgraded Django and added python-dotenv
    
    * Added Email service config
    
    * Added a new model Token
    
    * Updated model Token
    
    * Added Token serializer
    
    * Added email_utils.py
    
    * Added password_reset_view.py
    
    * Added BASE_URL env in settings.py
    
    * Updated token_serializer.py
    
    * Added password_reset templates
    
    * Added password_reset url
    
    * Updated password_reset url
    
    * Updated PasswordRecoveryForm.js
    
    * Renamed to PasswordForgot
    
    * Added PasswordReset.js
    
    * Added password_reset_view.py
    
    * Added password_urls.py
    
    * Updated urls.py
    
    * Updated PasswordRecoveryForm.js
    
    * Updated PasswordReset.js

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,5 @@
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = []
+urlpatterns = [
+    path("password/", include("api.custom_urls.password_urls"), name="password"),
+]
