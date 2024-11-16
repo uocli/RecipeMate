8093768ee9b7042836e7cfb8b5c72dfa329e978d
Sean Li <cli049@uottawa.ca>
2024-11-16 16:37:08 -0500

    Added password_reset_view.py

---
+++ backend/api/custom_views/password_reset_view.py
@@ -0,0 +1,74 @@
+from datetime import timedelta
+import hashlib
+from urllib.parse import urljoin
+import uuid
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..serializers.token_serializer import TokenSerializer
+from ..utils.email_utils import send_email
+
+
+class PasswordResetView(APIView):
+    def post(self, request, format=None):
+        email = request.data["email"]
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return Response(
+                {
+                    "success": False,
+                    "message": "User with this email does not exist.",
+                },
+                status=status.HTTP_200_OK,
+            )
+
+        created_at = timezone.now()
+        expires_at = timezone.now() + timedelta(days=1)
+        salt = uuid.uuid4().hex
+        token = hashlib.sha512(
+            (str(user.id) + user.password + created_at.isoformat() + salt).encode(
+                "utf-8"
+            )
+        ).hexdigest()
+        token_obj = {
+            "token": token,
+            "created_at": created_at,
+            "expires_at": expires_at,
+            "user": user.id,
+        }
+
+        serializer = TokenSerializer(data=token_obj)
+        if serializer.is_valid():
+            serializer.save()
+            # Send email
+            subject = "Password Reset Request Received!"
+            url = urljoin(
+                settings.BASE_URL, f"/password-reset?id={user.id}&token={token}"
+            )
+            context = {
+                "url": url,
+            }
+            send_email(subject, email, "password_reset", context)
+            return Response(
+                {
+                    "success": True,
+                    "message": "A password reset link has been sent to your email.",
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            error_msg = ""
+            for key in serializer.errors:
+                error_msg += serializer.errors[key][0]
+            return Response(
+                {
+                    "success": False,
+                    "message": error_msg,
+                },
+                status=status.HTTP_200_OK,
+            )
