5d64ad6348c08fbcf0e0b7a809dca451751259d3
Sean Li <cli049@uottawa.ca>
2024-11-15 20:52:54 -0500

    Updated ProtectedRoute.js to verify token when a protected view is accessed

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,13 +1,55 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated, authTokens, logout, setAuthTokens } = useContext(AuthContext);
     const location = useLocation();
-    const { isAuthenticated } = useContext(AuthContext);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const verifyToken = async () => {
+            if (authTokens) {
+                try {
+                    await axios.post(
+                        "/auth/token/verify/",
+                        {},
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${authTokens.access}`,
+                                "X-CSRFToken": Cookies.get("csrftoken"),
+                            },
+                        }
+                    );
+                    setLoading(false);
+                } catch {
+                    try {
+                        const response = await axios.post("/auth/token/refresh/", {
+                            refresh: authTokens.refresh,
+                        });
+                        setAuthTokens(response.data);
+                        Cookies.set("access_token", response.data.access);
+                        setLoading(false);
+                    } catch {
+                        logout();
+                        setLoading(false);
+                    }
+                }
+            } else {
+                setLoading(false);
+            }
+        };
+        verifyToken();
+    }, [authTokens, logout, setAuthTokens]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     if (!isAuthenticated) {
-        // Redirect to login page with the original location saved in state
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
