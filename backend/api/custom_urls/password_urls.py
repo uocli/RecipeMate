c870d462a464abac5185ad1d5c661f01118bd4ec
Sean Li <cli049@uottawa.ca>
2024-11-18 10:32:54 -0500

    Added password_change_view.py

--- backend/api/custom_urls/password_urls.py
+++ backend/api/custom_urls/password_urls.py
@@ -5,7 +5,7 @@ from ..custom_views.password_forgot_view import PasswordForgotView
 from ..custom_views.password_reset_view import ResetPasswordView
 
 urlpatterns = [
-    path("forgot/", PasswordForgotView.as_view(), name="forgot_password"),
     path("change/", PasswordChangeView.as_view(), name="change_password"),
+    path("forgot/", PasswordForgotView.as_view(), name="forgot_password"),
     path("reset/", ResetPasswordView.as_view(), name="reset_password"),
 ]
