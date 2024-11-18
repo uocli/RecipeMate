c792ee77016d6997335bd1648546e1a72ed5d529
Sean Li <cli049@uottawa.ca>
2024-11-18 11:18:26 -0500

    Updated token_views.py to handle success and failure

--- backend/auth/custom_views/token_views.py
+++ backend/auth/custom_views/token_views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,8 +10,14 @@ class VerifyTokenView(APIView):
     ]
 
     def post(self, request, *args, **kwargs):
-        # If the token is valid, the request will be authenticated
         user = request.user  # The authenticated user
-
-        return Response({"valid": True, "user_id": user.id, "username": user.username})
-
+        if user.is_authenticated:
+            return Response(
+                {"success": True},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"success": False},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
