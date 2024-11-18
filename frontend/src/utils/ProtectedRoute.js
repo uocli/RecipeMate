b96caba15eae7820f0b2d461ca70ebcf75d7365d
Sean Li <cli049@uottawa.ca>
2024-11-18 11:18:54 -0500

    Updated ProtectedRoute.js to use axios hook

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,19 +1,21 @@
 import { useContext, useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
-import axios from "axios";
 import Cookies from "js-cookie";
+import useAxios from "./useAxios";
 
 const ProtectedRoute = ({ children }) => {
-    const { isAuthenticated, authTokens, logout, setAuthTokens } = useContext(AuthContext);
+    const axios = useAxios();
+    const { isAuthenticated, authTokens, logout, setAuthTokens, setUser } =
+        useContext(AuthContext);
     const location = useLocation();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const verifyToken = async () => {
             if (authTokens) {
-                try {
-                    await axios.post(
+                axios
+                    .post(
                         "/auth/token/verify/",
                         {},
                         {
@@ -22,27 +24,37 @@ const ProtectedRoute = ({ children }) => {
                                 Authorization: `Bearer ${authTokens.access}`,
                                 "X-CSRFToken": Cookies.get("csrftoken"),
                             },
+                        },
+                    )
+                    .then((response) => {
+                        const { status, data } = response || {},
+                            { success, data: user } = data || {};
+                        if (status === 200 && success) {
+                            setUser(user);
+                        } else {
+                            // refresh token
+                            axios
+                                .post("/auth/token/refresh/", {
+                                    refresh: authTokens.refresh,
+                                })
+                                .then((response) => {
+                                    setAuthTokens(response.data);
+                                    Cookies.set(
+                                        "access_token",
+                                        response.data.access,
+                                    );
+                                });
                         }
-                    );
-                    setLoading(false);
-                } catch {
-                    try {
-                        const response = await axios.post("/auth/token/refresh/", {
-                            refresh: authTokens.refresh,
-                        });
-                        setAuthTokens(response.data);
-                        Cookies.set("access_token", response.data.access);
-                        setLoading(false);
-                    } catch {
-                        logout();
-                        setLoading(false);
-                    }
-                }
-            } else {
-                setLoading(false);
+                    });
             }
         };
-        verifyToken();
+        verifyToken()
+            .catch(() => {
+                logout();
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, [authTokens, logout, setAuthTokens]);
 
     if (loading) {
