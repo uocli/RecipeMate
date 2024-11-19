0ad74edc5bbb78d8a9f91d3cb2de9b21a42cc405
Sean Li <cli049@uottawa.ca>
2024-11-18 20:57:12 -0500

    Fixed user registration issue where username was not saved

--- backend/auth/custom_views/registration_view.py
+++ backend/auth/custom_views/registration_view.py
@@ -3,18 +3,23 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from auth.serializers.user_serializer import UserSerializer
+from ..serializers.user_registration_serializer import UserRegistrationSerializer
 
 
 class RegistrationView(APIView):
+    user_registration_serializer = UserRegistrationSerializer
+
     def post(self, request, format=None):
         request.data["password"] = make_password(password=request.data["password"])
         request.data["username"] = request.data["email"]
-        serializer = UserSerializer(data=request.data)
+        serializer = self.user_registration_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(
-                {"success": True, "message": "You are now registered on our website!"},
+                {
+                    "success": True,
+                    "message": "You hae been successfully registered!",
+                },
                 status=status.HTTP_200_OK,
             )
         else:
