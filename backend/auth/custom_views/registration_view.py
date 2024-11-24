7465bb481b2bb96837124dd51ca890e4295af88c
Sean Li <cli049@uottawa.ca>
2024-11-24 09:58:28 -0500

    Updated registration_view.py to send an invitation for registration

--- backend/auth/custom_views/registration_view.py
+++ backend/auth/custom_views/registration_view.py
@@ -1,24 +1,74 @@
-from django.contrib.auth.hashers import make_password
+import hashlib
+import uuid
+from datetime import timedelta
+from urllib.parse import urljoin
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..serializers.user_registration_serializer import UserRegistrationSerializer
+from backend import settings
+from api.serializers.token_serializer import TokenSerializer
+from api.utils.email_utils import send_email
 
 
-class RegistrationView(APIView):
-    user_registration_serializer = UserRegistrationSerializer
+@method_decorator(csrf_exempt, name="dispatch")
+class SendInviteView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, format=None):
+        return Response(
+            {
+                "success": False,
+                "message": "Method not allowed.",
+            },
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
 
     def post(self, request, format=None):
-        request.data["password"] = make_password(password=request.data["password"])
-        request.data["username"] = request.data["email"]
-        serializer = self.user_registration_serializer(data=request.data)
+        email = request.data["email"]
+        user = User.objects.filter(email=email).first()
+        if user is not None:
+            return Response(
+                {
+                    "success": False,
+                    "message": "A user with this email already exists!",
+                },
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        created_at = timezone.now()
+        expires_at = timezone.now() + timedelta(days=1)
+        salt = uuid.uuid4().hex
+        token = hashlib.sha512(
+            (email + created_at.isoformat() + salt).encode("utf-8")
+        ).hexdigest()
+        token_obj = {
+            "token": token,
+            "created_at": created_at,
+            "expires_at": expires_at,
+            "email": email,
+        }
+
+        serializer = TokenSerializer(data=token_obj)
         if serializer.is_valid():
             serializer.save()
+            # Send email
+            subject = "Welcome to Recipe Mate!"
+            url = urljoin(settings.BASE_URL, f"/complete-signup?token={token}")
+            context = {
+                "url": url,
+            }
+            send_email(subject, email, "invite_email", context)
             return Response(
                 {
                     "success": True,
-                    "message": "You hae been successfully registered!",
+                    "message": "An invite link has been sent to your email!",
                 },
                 status=status.HTTP_200_OK,
             )
@@ -27,6 +77,9 @@ class RegistrationView(APIView):
             for key in serializer.errors:
                 error_msg += serializer.errors[key][0]
             return Response(
-                {"success": False, "message": error_msg},
-                status=status.HTTP_200_OK,
+                {
+                    "success": False,
+                    "message": error_msg,
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 
 from .custom_views.login_view import LoginView
 from .custom_views.logout_view import LogoutView
-from .custom_views.registration_view import RegistrationView
+from .custom_views.registration_view import SendInviteView
 from .custom_views.csrf_token_view import CSRFTokenView
 
 urlpatterns = [
     path("csrf-token/", CSRFTokenView.as_view(), name="csrf-token"),
     path("token/", include("auth.custom_urls.token_urls")),
-    path("signup/", RegistrationView.as_view(), name="signup"),
+    path("send-invite/", SendInviteView.as_view(), name="send-invite"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
