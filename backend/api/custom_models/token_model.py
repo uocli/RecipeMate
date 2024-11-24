f02cafd0b2575ba40d88d9af77394942e2c760fe
Sean Li <cli049@uottawa.ca>
2024-11-24 09:58:53 -0500

    Updated token_model.py to allow empty user_id since for registration the user_id is not ready yet

--- backend/api/custom_models/token_model.py
+++ backend/api/custom_models/token_model.py
@@ -10,5 +10,11 @@ class Token(models.Model):
     token = models.CharField(max_length=255)
     created_at = models.DateTimeField()
     expires_at = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tokens")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="tokens",
+        null=True,
+        blank=True,
+    )
     is_used = models.BooleanField(default=False)
