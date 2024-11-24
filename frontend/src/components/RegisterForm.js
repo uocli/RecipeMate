fd3bfbbf7d248bcfe665b1568618a48140a49309
Sean Li <cli049@uottawa.ca>
2024-11-24 09:59:20 -0500

    Updated RegisterForm.js to reflect the changes

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -6,7 +6,7 @@ import { AuthContext } from "../utils/AuthContext";
 import { AlertContext } from "../utils/AlertContext";
 
 const RegisterForm = () => {
-    const [formData, setFormData] = useState({ email: "", password: "" });
+    const [email, setEmail] = useState("");
     const navigate = useNavigate();
     const { isAuthenticated } = useContext(AuthContext);
     const { showAlert } = useContext(AlertContext);
@@ -22,48 +22,37 @@ const RegisterForm = () => {
         return regex.test(email);
     };
 
-    const validatePassword = (password) => {
-        const regex = /.*/;
-        // /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return regex.test(password);
-    };
-
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        setEmail(e.target.value);
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const { email, password } = formData;
         if (!validateEmail(email)) {
-            showAlert("Invalid email format", "error");
-            return;
-        }
-
-        if (!validatePassword(password)) {
-            showAlert(
-                "Password must contain uppercase, lowercase, numbers, special characters, and be at least 8 characters long.",
-                "error",
-            );
+            showAlert("Invalid email format!", "error");
             return;
         }
 
         axios
-            .post("/auth/signup/", formData)
+            .post("/auth/send-invite/", { email })
             .then((response) => {
                 if (response.status === 200) {
-                    const { success, message } = response.data || {};
-                    if (success) {
-                        showAlert(message, "success", 5000);
-                        navigate("/login");
-                        setFormData({ email: "", password: "" });
-                    } else {
-                        showAlert(message, "error");
-                    }
+                    showAlert(
+                        "Invite email sent successfully!",
+                        "success",
+                        5000,
+                    );
+                    setEmail("");
+                } else {
+                    showAlert("Failed to send invite email!", "error");
                 }
             })
             .catch((error) => {
-                showAlert(error.response?.data?.message, "error");
+                showAlert(
+                    error.response?.data?.message ||
+                        "Error sending invite email",
+                    "error",
+                );
             });
     };
 
@@ -76,53 +65,39 @@ const RegisterForm = () => {
             <Typography variant="h5" align="center" gutterBottom>
                 Register
             </Typography>
-
-            <>
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    required
-                />
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    sx={{ marginTop: 2 }}
+            <TextField
+                label="Email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                required
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                sx={{ marginTop: 2 }}
+            >
+                Register
+            </Button>
+            <Typography
+                variant="body2"
+                align="center"
+                style={{ marginTop: "16px" }}
+            >
+                Already have an account?{" "}
+                <Link
+                    component="button"
+                    underline="hover"
+                    onClick={() => navigate("/login")}
                 >
-                    Register
-                </Button>
-                <Typography
-                    variant="body2"
-                    align="center"
-                    style={{ marginTop: "16px" }}
-                >
-                    Already have an account?{" "}
-                    <Link
-                        component="button"
-                        underline="hover"
-                        onClick={() => navigate("/login")}
-                    >
-                        Login
-                    </Link>
-                </Typography>
-            </>
+                    Login
+                </Link>
+            </Typography>
         </Box>
     );
 };
