e12e5230d7ea21a4b9a39e29364dcef1c8d0d48d
Sean Li <cli049@uottawa.ca>
2024-11-25 11:28:24 -0500

    Updated access_token to be available for 2 hours and refresh_token for 1 day

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -194,8 +194,8 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 
 # Simple JWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
