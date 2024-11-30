7b3f06c292079291100bc851f936e79167e7ce70
Sean Li <cli049@uottawa.ca>
2024-11-30 16:02:46 -0500

    Added RecipeCreateView

--- backend/api/custom_views/recipe_views.py
+++ backend/api/custom_views/recipe_views.py
@@ -1,9 +1,13 @@
+import json
+
 from django.db.models import Avg, Value, FloatField
 from django.db.models.functions import Coalesce
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..custom_models.public_recipe_models import PublicRecipe, Rating
+
+from ..custom_models.favorites_model import Favorite
+from ..custom_models.public_recipe_models import PublicRecipe, Rating, Ingredient
 from ..serializers.recipe_serializers import RecipeSerializer
 
 
@@ -65,3 +69,58 @@ class RecipeRateView(APIView):
                 {"message": str(e)},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+
+class RecipeCreateView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        user = request.user
+
+        try:
+            favorite = Favorite.objects.get(user=user, id=pk)
+        except Favorite.DoesNotExist:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Favorite recipe not found.",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        ingredients = favorite.ingredients
+        recipe_steps = favorite.recipe
+
+        if ingredients:
+            ingredients_data = json.loads(ingredients)
+            ingredient_names = [
+                ingredient["ingredient"].lower() for ingredient in ingredients_data
+            ]
+
+            # Create or get ingredients
+            ingredient_objects = []
+            for name in ingredient_names:
+                ingredient, created = Ingredient.objects.get_or_create(name=name)
+                ingredient_objects.append(ingredient)
+
+        instructions = ""
+        if recipe_steps:
+            instructions = "\n".join(json.loads(recipe_steps))
+
+        # Create the public recipe
+        public_recipe = PublicRecipe.objects.create(
+            name=favorite.name,
+            description=favorite.name,
+            instructions=instructions,
+        )
+
+        # Add ingredients to the public recipe
+        public_recipe.ingredients.set(ingredient_objects)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Public recipe created successfully.",
+            },
+            status=status.HTTP_201_CREATED,
+        )
